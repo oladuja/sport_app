46ef343ddafa51915adad57b79220b0989e0c3ae
oladuja <oladuja13@gmail.com>
2023-06-26 22:42:23 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main () => runApp(const SportApp());
+
+class SportApp extends StatelessWidget {
+  const SportApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // home: HomeScreen(),
+    );
+  }
+}
+
+
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeS extends StatelessWidget {
+  const HomeS({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
